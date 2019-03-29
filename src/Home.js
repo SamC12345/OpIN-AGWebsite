fb6e444339a7cd9e7522cdb7be972edfae6e669b
Sam C <student979@live.com>
2019-03-29 18:20:26 -0400

    preliminary background image

--- src/Home.js
+++ src/Home.js
@@ -4,10 +4,10 @@ import logo from './vertical-farming-image.jpg';
 class Home extends Component {
   render() {
     return (
-      <div>
+      <div  style={{backgroundImage: `url(${logo})`}}>
         <h1>O-PIN Agriculture Initiative</h1>
         <h3><sub>Inspiring and Empowering the High Tech Farmers of the Future.</sub></h3><br></br>
- <img src={logo} style={{height:"200px"}} ></img>
+ {/* <img src={logo} style={{height:"200px"}} ></img> */}
       </div>
     );
   }
