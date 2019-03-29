fab663dba89afe85e3da2926b01c39767f61b9e8
Daniel A Kontoh-Boateng <kontohda@shu.edu>
2019-03-29 19:02:47 -0400

    home

--- src/Home.js
+++ src/Home.js
@@ -4,9 +4,10 @@ import logo from './vertical-farming-image.jpg';
 class Home extends Component {
   render() {
     return (
-      <div  style={{backgroundImage: `url(${logo})`, height:"85vh", backgroundSize:"contain", color:"wheat", textAlign:"center"}}>
-        <h1>O-PIN Agriculture Initiative</h1>
-        <h3><sub>Inspiring and Empowering the High Tech Farmers of the Future.</sub></h3>
+      <div  style={{backgroundImage: `url(${logo})`, height:"85vh", backgroundSize:"cover", color:"white", textAlign:"center", backgroundRepeat: "no-repeat"}}>
+        <br></br><br></br><br></br>
+        <h1> <font size="15" face="baufra">O-PIN Agriculture Initiative</font></h1>
+        <h3><font size="5" face="baufra">Inspiring and Empowering the High Tech Farmers of the Future.</font></h3>
         <br></br>
  {/* <img src={logo} style={{height:"200px"}} ></img> */}
       </div>
