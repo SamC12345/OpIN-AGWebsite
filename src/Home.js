9b85123e07284c6fe6ef8aea05e27f5f0ffaa6ac
Sam C <student979@live.com>
2019-03-29 19:10:51 -0400

    added text shadows

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import logo from './vertical-farming-image.jpg';
 class Home extends Component {
   render() {
     return (
-      <div  style={{backgroundImage: `url(${logo})`, height:"85vh", backgroundSize:"cover", color:"white", textAlign:"center", backgroundRepeat: "no-repeat"}}>
+      <div  style={{backgroundImage: `url(${logo})`, height:"85vh", backgroundSize:"cover", color:"white", textAlign:"center", backgroundRepeat: "no-repeat", textShadow: "-2px 0 black, 0 2px black, 2px 0 black, 0 -2px black"}}>
         <br></br><br></br><br></br>
         <h1> <font size="15" face="baufra">O-PIN Agriculture Initiative</font></h1>
         <h3><font size="5" face="baufra">Inspiring and Empowering the High Tech Farmers of the Future.</font></h3>
